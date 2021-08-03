bbc61b1cbb3cf983906377d46a1af81a1ca30980
kofoworola <adenekankofoworola@gmail.com>
2021-08-03 19:06:57 +0100

    add new design to cartlist page

--- src/Components/Cartlist/index.js
+++ src/Components/Cartlist/index.js
@@ -1,47 +1,52 @@
-import React from 'react'
+import React from "react";
+import './index.css'
 
 const Cartlist = ({ cart, incrementqty, decrementqty }) => {
-  console.log(cart)
-    return (
-        <div>
-          {cart.map(c => {
-            return (
-              <div>
-            <div className="des">
-        <div className="des-image">
-          <img src={c.img} alt=""></img>
-        </div>
-        <div className="des-content">
-          <div>
-            <h1>{c.name}</h1>
-            <p>{c.description}</p>
-            <div className="line"></div>
-            <div className="des-price">&#8358;{c.price}</div>
-            <div className="line"></div>
-            <div className="qty">
-              <span style={{ marginRight: "10px" }}>Quantity :</span>
-              <table>
-                <tr>
-                  <td style={{ cursor: "pointer" }} onClick={()=>decrementqty(c.id)}>-</td>
-                  <td>{c.qty}</td>
-                  <td style={{ cursor: "pointer" }} onClick={()=>incrementqty(c.id)}>+</td>
-                </tr>
-              </table>
+  console.log(cart);
+  return (
+    <div className='wrapper'>
+      {cart.map((c) => {
+        return (
+          <>
+            <div className="sub-wrapper">
+              <div className="cart-image">
+                <img src={c.img} alt="" />
+              </div>
+              <div className="des-content">
+                <div>
+                  <h3>{c.name}</h3>
+                  <p>{c.description}</p>
+                  <div className="cart-price">&#8358;{c.price}</div>
+                  <div className="quantity">
+                    <span style={{ marginRight: "10px" }}>Quantity :</span>
+                    <table>
+                      <tr>
+                        <td
+                          style={{ cursor: "pointer" }}
+                          onClick={() => decrementqty(c.id)}
+                        >
+                          -
+                        </td>
+                        <td>{c.qty}</td>
+                        <td
+                          style={{ cursor: "pointer" }}
+                          onClick={() => incrementqty(c.id)}
+                        >
+                          +
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+                <button className="btn">Buy Now</button>
+              </div>
             </div>
-            <div className="line"></div>
-            <button
-              className="des-btn"
-            >
-              Buy Now
-            </button>
-          </div>
-        </div>
-      </div>
-        </div>
-            )
-          })}
-        </div>
-    )
-}
+          </>
+        );
+      }) 
+    }
+    </div>
+  );
+};
 
-export default Cartlist
+export default Cartlist;
